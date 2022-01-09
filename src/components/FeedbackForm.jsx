536a31cfde3eff489a0d143719c6917ee83c957c
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-01-10 04:41:33 +0500

    stuck

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
 import Card from "../shared/Card";
+import Button from "../shared/Button";
+import RatingSelect from "./RatingSelect";
 
 const FeedbackForm = () => {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage("Text must be at least 10 characters");
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
     setText(e.target.value);
   };
 
@@ -12,11 +27,14 @@ const FeedbackForm = () => {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
-        {/* todo - rating select component*/}
+        <RatingSelect />
         <div className="input-group">
           <input type="text" onChange={handleChange} value={text} />
-          <button type="submit">Send</button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
