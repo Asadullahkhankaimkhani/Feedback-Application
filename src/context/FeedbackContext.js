2701b50ab486585fa18ebae392273748988634a1
unknown <asadullahk15@gmail.com>
2022-04-17 16:43:55 +0500

    Fetch Data from Json Server backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,26 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This item from context is from FeedbackProvider",
-      rating: 5,
-    },
-    {
-      id: 2,
-      text: "This item from context is from FeedbackProvider",
-      rating: 10,
-    },
-    {
-      id: 3,
-      text: "This item from context is from FeedbackProvider",
-      rating: 7,
-    },
-  ]);
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  const fetchFeedback = async () => {
+    const response = await fetch("http://localhost:5000/feedback");
+    const feedback = await response.json();
+    setFeedback(feedback);
+    setIsLoading(false);
+  };
 
   const feedbackDelete = (id) => {
     if (window.confirm("Are You Sure?"))
